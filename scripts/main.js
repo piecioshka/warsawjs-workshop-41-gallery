b83f3c7f14a6a84f6121402605931841bd4bf064
Piotr Kowalski <piecioshka@gmail.com>
2020-02-23 12:41:13 +0100

    Refactoring — split to smaller functions

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,25 @@
+function renderPhoto(photo) {
+    console.log('photo', photo);
+
+    // Create HTMLImageElement
+    const $photo = document.createElement('img');
+    // $photo.src = photo;
+    $photo.setAttribute('src', photo);
+
+    // Render
+    const $photos = document.querySelector('#photos');
+    $photos.append($photo);
+}
+
+function renderPhotos(photos) {
+    // for (let i = 0; i < photos.length; i++) {
+    //     const photo = photos[i];
+    //     renderPhoto(photo);
+    // }
+    photos.forEach(function (photo) { // Anonymous Function
+        renderPhoto(photo);
+    });
+}
 
 // Function Declaration
 function main() {
@@ -12,25 +34,7 @@ function main() {
         "https://i.picsum.photos/id/1035/200/300.jpg",
         "https://i.picsum.photos/id/1033/200/300.jpg",
     ];
-
-    for (let i = 0; i < photos.length; i++) {
-        const photo = photos[i];
-        console.log('photo', i, photo);
-
-        // Create HTMLImageElement
-        const $photo = document.createElement('img');
-        // $photo.src = photo;
-        $photo.setAttribute('src', photo);
-
-        // Render
-        const $photos = document.querySelector('#photos');
-        $photos.append($photo);
-    }
-
-    // array.forEach(element => {
-
-    // });
-
+    renderPhotos(photos);
 
 }
 
